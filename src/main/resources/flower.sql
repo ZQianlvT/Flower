929b9635bc6b29473cc4a6550c6c7397ccf48202
OrionPax <ZQianlvT@outlook.com>
2018-05-23 08:44:18 +0800

    sql语句

--- src/main/resources/flower.sql
+++ src/main/resources/flower.sql
@@ -1,7 +1,7 @@
 ﻿CREATE DATABASE flower;
 USE flower;
 CREATE TABLE IF NOT EXISTS f_user(
-	u_id VARCHAR(40),
+	u_id VARCHAR(40) PRIMARY KEY,
 	u_email VARCHAR(40) UNIQUE,
 	u_pwd VARCHAR(40),
 	u_name VARCHAR(20),
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS f_user(
 );
 
 CREATE TABLE IF NOT EXISTS f_team(
-	t_id VARCHAR(40),
+	t_id VARCHAR(40) PRIMARY KEY,
 	t_name VARCHAR(40),
 	t_create_time DATETIME,
 	t_total_members INT,
@@ -19,13 +19,13 @@ CREATE TABLE IF NOT EXISTS f_team(
 );
 
 CREATE TABLE if NOT EXISTS f_userandteam(
-	uat_id VARCHAR(40),
+	uat_id VARCHAR(40) PRIMARY KEY,
 	uat_u_id VARCHAR(40),
 	uat_t_id VARCHAR(40)
 );
 
 CREATE TABLE IF NOT EXISTS f_project(
-	p_id VARCHAR(40),
+	p_id VARCHAR(40) PRIMARY KEY,
 	p_name VARCHAR(20),
 	p_remark VARCHAR(120),
 	p_status INT,				-- 0未归档 1归档
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS f_project(
 );
 
 CREATE TABLE IF NOT EXISTS f_discussion(
-	d_id VARCHAR(40),
+	d_id VARCHAR(40) PRIMARY KEY,
 	d_name VARCHAR(40),
 	d_remark VARCHAR(120),
 	d_start_time DATETIME,
@@ -43,7 +43,7 @@ CREATE TABLE IF NOT EXISTS f_discussion(
 );
 
 CREATE TABLE IF NOT EXISTS f_reply(
-	r_id VARCHAR(40),
+	r_id VARCHAR(40) PRIMARY KEY,
 	r_remark VARCHAR(260),
 	r_reply_time DATETIME,
 	r_u_id VARCHAR(40),
@@ -51,7 +51,7 @@ CREATE TABLE IF NOT EXISTS f_reply(
 );
 
 CREATE TABLE IF NOT EXISTS f_schedule(
-	s_id VARCHAR(40),
+	s_id VARCHAR(40) PRIMARY KEY,
 	s_name VARCHAR(40),
 	s_status INT,			-- 0 全天类型 1普通类型
 	s_start_time DATETIME,
@@ -60,22 +60,26 @@ CREATE TABLE IF NOT EXISTS f_schedule(
 );
 
 CREATE TABLE IF NOT EXISTS f_userAndSchedule(
-	uas_id VARCHAR(40),
+	uas_id VARCHAR(40) PRIMARY KEY,
 	uas_s_id VARCHAR(40),
 	uas_u_id VARCHAR(40)
 );
 
 CREATE TABLE IF NOT EXISTS f_board(
-	b_id VARCHAR(40),
+	b_id VARCHAR(40) PRIMARY KEY,
 	b_name VARCHAR(40),
 	b_unfinished INT,
 	b_p_id VARCHAR(40)
 );
 
 CREATE TABLE IF NOT EXISTS f_task(
-	t_id VARCHAR(40),
+	t_id VARCHAR(40) PRIMARY KEY,
 	t_name VARCHAR(40),
 	t_end_time DATETIME,
 	t_u_id VARCHAR(40),
 	t_b_id VARCHAR(40)
 );
+
+/*
+DROP DATABASE flower;
+*/
