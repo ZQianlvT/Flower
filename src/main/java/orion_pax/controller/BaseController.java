bee9ef220cf0d550e241226c1688609e2f0c489c
OrionPax <ZQianlvT@outlook.com>
2018-05-25 10:20:42 +0800

    TeamService getByUId

--- src/main/java/orion_pax/controller/BaseController.java
+++ src/main/java/orion_pax/controller/BaseController.java
@@ -3,15 +3,20 @@ package orion_pax.controller;
 import javax.annotation.Resource;
 import javax.servlet.ServletContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import orion_pax.service.TeamService;
 
 @Controller
 @RequestMapping("/base")
 public class BaseController {
     @Resource
     public ServletContext application;
+
+    @Autowired
+    protected TeamService teamService;
 
     @RequestMapping("/goURL/{folder}/{file}")
     public String goURL(@PathVariable String folder,@PathVariable String file){
--- src/main/java/orion_pax/service/impl/TeamServiceImpl.java
+++ src/main/java/orion_pax/service/impl/TeamServiceImpl.java
@@ -1,9 +1,11 @@
 package orion_pax.service.impl;
 
+import org.springframework.stereotype.Service;
 import orion_pax.entity.Team;
 import orion_pax.entity.User;
 import orion_pax.service.TeamService;
 
+@Service("TeamService")
 public class TeamServiceImpl extends BaseServiceImpl<Team> implements TeamService {
     /**
      * 通过管理员id查询Team对象
